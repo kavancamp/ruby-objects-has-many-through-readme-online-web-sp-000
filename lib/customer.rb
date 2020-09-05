a5502e9c57774362078ce1aa804a1667667d0e07
Keenah Vancampenhout <kvancamp@mail.com>
2020-09-05 23:59:06 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,5 +1,6 @@
 class Customer
- attr_accessor :name, :age
+  attr_accessor :name, :age
+
   @@all = []
 
   def initialize(name, age)
@@ -12,36 +13,37 @@ class Customer
     @@all
   end
 
+  def meals
+    Meal.all.select do |meal|
+      meal.customer == self
+    end
+  end
+
+  def waiters
+    meals.map do |meal|
+      meal.waiter
+    end
+  end
+
   def new_meal(waiter, total, tip=0)
     Meal.new(waiter, self, total, tip)
   end
 
- def meals
-   Meal.all.select do |meal|
-     meal.customer == self
-   end
+  def new_meal_20_percent(waiter, total)
+    tip = total * 0.2
+    Meal.new(waiter, self, total, tip)
+  end
 
-   def waiters
-     meals.map do |meal|
-       meal.waiter
-     end
-   end
-
-   def new_meal_20_percent(waiter, total)
-     tip = total * 0.2
-     Meal,new(waiter, self, total, tip)
-   end
-
-   def self.oldest_customer
-     oldest_age = 0
-     oldest_customer = nil
-     self.all.each do |customer|
-       if customer.age > oldest_age
-          oldest_age = customer.age
-          oldest_customer = customer
-        end
+  def self.oldest_customer
+    oldest_age = 0
+    oldest_customer = nil
+    self.all.each do |customer|
+      if customer.age > oldest_age
+        oldest_age = customer.age
+        oldest_customer = customer
       end
-      oldest_customer
     end
+    oldest_customer
+  end
 
 end

3ffaa6724422bf092b74dccffccb651c29a9cc23
Keenah Vancampenhout <kvancamp@mail.com>
2020-09-05 23:13:19 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -34,7 +34,7 @@ class Customer
     oldest_customer = nil
     self.all.each do |customer|
       if customer.age > oldest_age
-        oldest_age = customer.age 
+        oldest_age = customer.age
         oldest_customer = customer
       end
     end
@@ -44,5 +44,5 @@ class Customer
   end
 
 
-  
+
 end
